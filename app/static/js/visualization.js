d11a899d791a45148b988ffc846bb6b4c47182c4
moumitaghanti <51742808+moumitaghanti@users.noreply.github.com>
2019-12-02 00:28:37 -0800

    added changes for plotly with latest date

--- app/static/js/visualization.js
+++ app/static/js/visualization.js
@@ -42,6 +42,8 @@ var tag1 = politicianDict[sel1];
 var queryurl1 = window.location.href + 'api/hashtag/' + tag1;
 var tag2 = politicianDict[sel2];
 var queryurl2 = window.location.href + 'api/hashtag/'+tag2;
+var queryurlplot1 = window.location.href + 'api/historical/'+tag1;
+var queryurlplot2 = window.location.href + 'api/historical/'+tag2;
 console.log(tag1);
 console.log(tag2);
 console.log(queryurl1);
@@ -52,7 +54,7 @@ d3.select("#keyInputs").remove()
 
 
  //filterdata(queryurl1,queryurl2);
- updatemap(queryurl1,queryurl2,tag1,tag2);
+ updatemap(queryurl1,queryurl2,tag1,tag2,queryurlplot1,queryurlplot2);
 
 };
 
@@ -133,7 +135,7 @@ d3.json(turl).then(function(data)
 };
 buildmap()
 
-function updatemap(queryurl1,queryurl2,tag1,tag2) {
+function updatemap(queryurl1,queryurl2,tag1,tag2,queryurlplot1,queryurlplot2) {
   //document.getElementById('map').innerHTML = "";
   
   
@@ -232,73 +234,164 @@ d3.json(queryurl1).then(function(data1){
   
   
  L.control.layers(baseMaps,overlayMaps).addTo(myMap);
-//});
-//});
+});
+});
  
+function fDate(s) {
+  var d = new Date();
+  s = s.split('/');
+  d.setFullYear(s[2]);
+  d.setMonth(s[1]);
+  d.setDate(s[0]);
+  return d;
+}
 
 //d3.csv(furl1).then(function(data1){
    
   //console.log(data1);
+d3.json(queryurlplot1).then(function(data1){  
+  console.log(data1) 
   var date_array1 =[];
-  var fav_twtcnt1 =[];
+  var avg_twtcnt1 =[];
   for (var i = 0; i < data1.length; i++) {
     var date1 = data1[i].Tweet_Created_At;
     var ftwtcnt1 = data1[i].Favorite_Count;
+    var retwtcnt1 = data1[i].Retweet_Count;
+    var tottwtcnt1 = (ftwtcnt1+retwtcnt1)/2;
     console.log(data1[i]);
      //var keyword = (data[i].keyword).slice(1);
     //console.log((data[i].keyword).slice(1));
     date_array1.push(date1);
-    fav_twtcnt1.push(ftwtcnt1);
+    avg_twtcnt1.push(tottwtcnt1);
     
   };
-  console.log(date_array1);
-  var trace1 = {
-    x: date_array1,
-    y: fav_twtcnt1,
-    //mode: "markers",
-    type: "line",
-    name: layername1,
-    //marker: {
-     // color: "blue",
-      //symbol: "cross"
-    //}
-  };
-  //d3.csv("assets/data/data_2.csv").then(function(data2){
-  //d3.csv("assets/data/data_2.csv", function(data2) {
+  console.log(date_array1.length);
   
-    
-    //console.log(date_array1);
+
+ 
+  
+  d3.json(queryurlplot2).then(function(data2){   
+    console.log(data2)  
     var date_array2 =[];
-    var fav_twtcnt2 =[];
+    var avg_twtcnt2 =[];
     for (var j = 0; j < data2.length; j++) {
       var date2 = data2[j].Tweet_Created_At;
       var ftwtcnt2 = data2[j].Favorite_Count;
+      var retwtcnt2 = data2[j].Retweet_Count;
+      var tottwtcnt2 = (ftwtcnt2+retwtcnt2)/2;
       console.log(data2[j]);
        //var keyword = (data[i].keyword).slice(1);
       //console.log((data[i].keyword).slice(1));
       date_array2.push(date2);
-      fav_twtcnt2.push(ftwtcnt2);
+      avg_twtcnt2.push(tottwtcnt2);
       
     };
-   console.log(date_array2);
+   console.log(date_array2.length);
+   console.log(avg_twtcnt1.length);
+   console.log(avg_twtcnt2.length);
+
+var max1 = date_array1[0],
+  min1 = date_array1[0];
+
+// iterate over array values and update min & max
+date_array1.forEach(function(v) {
+  max1 = new Date(v) > new Date(max1)? v: max1;
+  min1 = new Date(v) < new Date(min1)? v: min1;
+});
+
+console.log('max1 :', max1, 'min1 :', min1);
+
+
+  var max2 = date_array2[0],
+  min2 = date_array2[0];
+
+// iterate over array values and update min & max
+date_array2.forEach(function(v) {
+  max2 = new Date(v) > new Date(max2)? v: max2;
+  min2 = new Date(v) < new Date(min2)? v: min2;
+});
+
+console.log('max2 :', max2, 'min2 :', min2);
+
+  var set_date =[];
+  var earliest_date =[];
+  
+  if (new Date(min1) < new Date(min2))
+    {set_date = date_array2;
+      earliest_date = new Date(min2);
+  
+
+    console.log("2")}
+  else {set_date = date_array1;  
+    earliest_date = new Date(min1);
+  console.log("1")
+
+}
+  console.log(set_date);
+
+  console.log(earliest_date);
+  var date_test1=[];
+  var avg_testcnt1 =[];
+  for (var j = 0; j < data1.length; j++) {
+    var date1 = data1[j].Tweet_Created_At;
+    if ((new Date(date1)) >= (new Date(earliest_date))){
+    var ftwtcnt1 = data1[j].Favorite_Count;
+    var retwtcnt1 = data1[j].Retweet_Count;
+    var tottwtcnt1 = (ftwtcnt1+retwtcnt1)/2; 
+    date_test1.push(date1);
+    avg_testcnt1.push(tottwtcnt1);
+    }};
+    console.log(avg_testcnt1.length);
+    console.log(date_test1.length);
+    var date_test2=[];
+    var avg_testcnt2 =[];
+    for (var j = 0; j < data2.length; j++) {
+      var date2 = data2[j].Tweet_Created_At;
+      if ((new Date(date2)) >= (new Date(earliest_date))){
+      var ftwtcnt2 = data2[j].Favorite_Count;
+      var retwtcnt2 = data2[j].Retweet_Count;
+      var tottwtcnt2 = (ftwtcnt2+retwtcnt2)/2; 
+      date_test2.push(date2);
+      avg_testcnt2.push(tottwtcnt2);
+      }};
+      console.log(avg_testcnt2.length);
+      console.log(date_test2.length);
+     
+
+
+
+  var trace1 = {
+    x: date_test1,
+    y: avg_testcnt1,
+    mode: "lines",
+    type: "scatter",
+    name: tag1,
+    color: "blue"
+    // marker: {
+    //  color: "blue",
+    //   symbol: "cross"
+    // }
+  };
    var trace2 = {
-        x: date_array2,
-        y: fav_twtcnt2,
-        //mode: "markers",
-        type: "line",
-        name: layername2,
-        //marker: {
-         //color: "orange",
-          //symbol: "diamond-x"
-        //}
+        x: date_test2,
+        y: avg_testcnt2,
+        mode: "lines",
+        type: "scatter",
+        name: tag2,
+        color: "orange"
+        // marker: {
+        // color: "orange",
+        //  symbol: "diamond-x"
+        // }
       };
   
-var data = [trace1,trace2];
+var data = [trace1, trace2];
+
 
 var layout = {
-  title: layername1+" Vs. "+layername2,
-  xaxis: { title: "Date" },
-  yaxis: { title: "fav cnt" }
+  title: tag1+" Vs. "+tag2,
+  xaxis: { title: "Date"},
+  yaxis: { title: "Tweet Count" }
 };
 
 
